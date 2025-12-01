1a5480e746976f1155b979c291055da969651fc9
prateekarya100 <prateekarya100@gmail.com>
2025-12-01 20:21:54 +0530

    commit changes into eazybank

--- gatewayserver/src/main/java/com/tomcat/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/tomcat/gatewayserver/GatewayserverApplication.java
@@ -6,6 +6,8 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
+import java.time.LocalDateTime;
+
 @SpringBootApplication
 public class GatewayserverApplication {
 
@@ -14,23 +16,40 @@ public class GatewayserverApplication {
 	}
 
 	@Bean
-	public RouteLocator eazybankRouteConfig(RouteLocatorBuilder routeLocatorBuilder){
-		return routeLocatorBuilder.routes()
+	public RouteLocator eazybankRouteConfig(RouteLocatorBuilder builder){
+		return builder.routes()
 				.route(p -> p
 						.path("/eazybank/accounts/**")
-						.filters(f-> f.rewritePath("/eazybank/accounts/(?<segment>.*)","/${segment}"))
-						.uri("lb://ACCOUNTS"))
+						.filters(f -> f
+								.rewritePath("/eazybank/accounts/(?<segment>.*)", "/${segment}")
+								.addResponseHeader("X-ResponseTime", LocalDateTime.now().toString())
+								.addRequestHeader("X-Gateway", "EazyBank-GW")
+								.addRequestParameter("source", "gateway")
+//								.prefixPath("/api")  // adds /api before forwarding
+						)
+						.uri("lb://ACCOUNTS")
+				)
 				.route(p -> p
 						.path("/eazybank/cards/**")
-						.filters(f-> f.rewritePath("/eazybank/cards/(?<segment>.*)","/${segment}"))
-						.uri("lb://CARDS"))
+						.filters(f -> f
+								.rewritePath("/eazybank/cards/(?<segment>.*)", "/${segment}")
+								.addResponseHeader("X-ResponseTime", LocalDateTime.now().toString())
+//								.retry(config -> config.setRetries(3)) // retry failed requests
+						)
+						.uri("lb://CARDS")
+				)
 				.route(p -> p
 						.path("/eazybank/loans/**")
-						.filters(f-> f.rewritePath("/eazybank/loans/(?<segment>.*)","/${segment}"))
-						.uri("lb://LOANS"))
+						.filters(f -> f
+								.rewritePath("/eazybank/loans/(?<segment>.*)", "/${segment}")
+//								.circuitBreaker(c -> c
+//										.setName("loanCircuitBreaker")
+//										.setFallbackUri("forward:/loanFallback"))
+						)
+						.uri("lb://LOANS")
+				)
 				.build();
-
-
 	}
+
 
 }
